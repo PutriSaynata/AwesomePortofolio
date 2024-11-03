9f953ab9bce7ba360171c513fe4ae205c86f2105
PutriSaynata <saynataputri@gmail.com>
2024-11-04 06:21:07 +0700

    Acara 9

--- index.js
+++ index.js
@@ -3,8 +3,11 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './BottomNav';
 import {name as appName} from './app.json';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
 
 AppRegistry.registerComponent(appName, () => App);
 
